e9fca04c6260e38927c83f2d17b551d10d9a9a01
Nitin Kadam <99539536+nitinkadam70@users.noreply.github.com>
2022-08-30 22:46:23 +0530

    Update ApiRoutes.js

--- whatsappweb/src/utils/ApiRoutes.js
+++ whatsappweb/src/utils/ApiRoutes.js
@@ -1,4 +1,4 @@
-const host = "https://watsappwebapp.herokuapp.com";
+const host = process.env.REACT_APP_API_URL;
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
 export const allUsersRoutes = `${host}/api/auth/allusers`;
